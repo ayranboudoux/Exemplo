cff649c0bc628b3ee765dfb7d801da379b74f802
Ayran Boudoux <ayranboudoux.dev@gmail.com>
2026-05-14 19:31:10 -0300

    Alteraçoes basicas

--- app/src/main/java/com/example/myapplication/AdapterFila.java
+++ app/src/main/java/com/example/myapplication/AdapterFila.java
@@ -1,6 +1,37 @@
 package com.example.myapplication;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class AdapterFila extends RecyclerView.Adapter<AdapterFila.ViewHoldergit> {
+import java.util.List;
+
+public class AdapterFila extends RecyclerView.Adapter<AdapterFila.ViewHolder> {
+
+    private List<ItemForja> lista;
+
+    public AdapterFila(List<ItemForja> lista) {
+        this.lista = lista;
+    }
+
+    @NonNull
+    @Override
+    public AdapterFila.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(
+                android.R.layout.simple_list_item_1, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterFila.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
